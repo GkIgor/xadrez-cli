e01739924691a8deb5d28538c3b41496ab37a144
GkIgor <igorf0660@gmail.com>
2024-04-13 14:24:39 -0300

    feat: adicionando regras de tabuleiro e partida de xadrez

--- Program/src/boardgame/Board.java
+++ Program/src/boardgame/Board.java
@@ -4,35 +4,39 @@ public class Board {
   private int rows, columns;
   private Piece[][] pieces;
 
-  public Board(int rows, int columns){
+  public Board(int rows, int columns) {
     this.rows = rows;
     this.columns = columns;
     this.pieces = new Piece[rows][columns];
-  }
-
-  public void setColumns(int columns) {
-    this.columns = columns;
   }
 
   public int getColumns() {
     return columns;
   }
 
-  public void setRows(int rows) {
-    this.rows = rows;
+  public void setColumns(int columns) {
+    this.columns = columns;
   }
 
   public int getRows() {
     return rows;
   }
 
-  public Piece piece(int row, int column){
+  public void setRows(int rows) {
+    this.rows = rows;
+  }
+
+  public Piece piece(int row, int column) {
     return this.pieces[row][column];
   }
 
-  public Piece piece(Position position){
+  public Piece piece(Position position) {
     return pieces[position.getRow()][position.getColum()];
   }
 
+  public void placePiece(Piece piece, Position position) {
+    pieces[position.getRow()][position.getColum()] = piece;
+    piece.position = position;
+  }
 
 }
--- Program/src/chess/ChessMatch.java
+++ Program/src/chess/ChessMatch.java
@@ -1,15 +1,25 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
   private Board board;
 
   public ChessMatch() {
     this.board = new Board(8, 8);
+    this.initialSetup();
   }
 
-  public ChessPiece[][] getPieces(){
+  private void initialSetup() {
+    board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+    board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+    board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+  }
+
+  public ChessPiece[][] getPieces() {
     ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 
     for (int i = 0; i < board.getRows(); i++) {
@@ -23,8 +33,5 @@ public class ChessMatch {
     }
     return mat;
   }
-
-
-
 
 }
