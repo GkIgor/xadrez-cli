fa898b593fcd5d10631a65d92bd2fe59c1ad5b61
GkIgor <igorf0660@gmail.com>
2024-04-13 10:54:41 -0300

    feat: imprimindo tabuleiro

--- Program/src/Main.java
+++ Program/src/Main.java
@@ -1,8 +1,11 @@
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Main {
   public static void main(String[] args) {
-    Position position = new Position(3, 5);
-    System.out.println(position);
+
+    ChessMatch chessMatch = new ChessMatch();
+    UI.printBoard(chessMatch.getPieces());
+
   }
+
 }
